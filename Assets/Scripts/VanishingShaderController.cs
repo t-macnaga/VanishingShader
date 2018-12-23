5e77cd3b76d061d2986bdbdf7d257156e84ca496
macnaga <macnaga6446@gmail.com>
2018-12-23 18:32:00 +0900

    initial commit

---
+++ Assets/Scripts/VanishingShaderController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class VanishingShaderController : MonoBehaviour
+{
+    [SerializeField] float strengthX = 0f;
+    [SerializeField] float strengthY = 0f;
+    [SerializeField] float alpha = 1f;
+    [SerializeField] Material material;
+
+    void Update()
+    {
+        UpdateMaterial();
+    }
+
+    void UpdateMaterial()
+    {
+        if (material == null) { return; }
+
+        material.SetFloat("_StrengthX", strengthX);
+        material.SetFloat("_StrengthY", strengthY);
+        material.SetFloat("_Alpha", alpha);
+    }
+}
